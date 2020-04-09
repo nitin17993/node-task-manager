17fa3504382affb4f1bc7485c3912d2e604a3e75
Nitin2 Dixit <Nitin2.Dixit@ril.com>
2020-04-09 12:53:18 +0530

    Removed Sharp

--- src/routers/user.js
+++ src/routers/user.js
@@ -3,7 +3,7 @@ const router = new express.Router();
 const User = require('../models/user');
 const auth = require('../middleware/auth');
 const multer = require('multer');
-const sharp = require('sharp');
+// const sharp = require('sharp');
 const { sendWelcomeEmail, sendCancelationEmail } = require('../emails/account');
 
 // Create User
@@ -137,8 +137,9 @@ const upload = multer({
     }
 })
 router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
-    const buffer = await sharp(req.file.buffer).resize({width: 250, height: 250}).png().toBuffer();
-    req.user.avatar = buffer;
+    // const buffer = await sharp(req.file.buffer).resize({width: 250, height: 250}).png().toBuffer();
+
+    req.user.avatar = req.file.buffer;
     await req.user.save();
     res.send();
 }, (error, req, res, next) => {
